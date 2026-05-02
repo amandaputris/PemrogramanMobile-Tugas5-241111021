66e3ad18c8082eefcd824430760a38c62e6c1ea3
amandaputris <amanda.salsabilla@widyatama.ac.id>
2026-05-02 10:10:53 +0700

    first commit

---
+++ app/src/main/java/com/example/nutrifit/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.nutrifit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Hanya memberikan padding kiri dan kanan, biarkan NavHost dan BottomNav menangani insets mereka sendiri jika perlu
+            // atau atur padding bawah jika BottomNav tidak menanganinya secara otomatis.
+            v.setPadding(systemBars.left, 0, systemBars.right, 0)
+            insets
+        }
+
+        // Setup Navigation Component dengan Bottom Navigation
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
